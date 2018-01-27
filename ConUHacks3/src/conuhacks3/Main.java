5ea534d0d12ce22182e783a2e555a9e0139a6a08
Cristian Aldea <a.crist8@live.com>
2018-01-27 18:50:12 -0500

    Initial Git commit to setup the main window

---
+++ ConUHacks3/src/conuhacks3/Main.java
@@ -0,0 +1,12 @@
+package conuhacks3;
+
+import visual.MainWindow;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("This is the main file.");
+		MainWindow mainWindow = new MainWindow();
+	}
+
+}
---
+++ ConUHacks3/src/visual/MainWindow.java
@@ -0,0 +1,15 @@
+package visual;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+	public MainWindow() {
+		this.setSize(new Dimension(500, 500));
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+
+}
