0439dff72b015518c6750a1ee1bb9b1ea2c09848
Alex Cohen <alex.toutongi@gmail.com>
2018-01-27 20:26:11 -0500

    update packages

---
+++ ConUHacks3/src/logic/Question.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class Question {
+
+	public Question() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
