2cea22f89d2230b67ce7991e3504b70010fd0380
Alex Cohen <alex.toutongi@gmail.com>
2018-01-27 20:53:54 -0500

    Created a Question object

--- ConUHacks3/src/logic/Question.java
+++ ConUHacks3/src/logic/Question.java
@@ -1,14 +1,32 @@
 package logic;
 
 public class Question {
+	int time, term1, term2, result;
+	char op;
+	term missingTerm;
 
-	public Question() {
-		// TODO Auto-generated constructor stub
+	public Question(int term1, int term2, char op, int result, term missingTerm) {
+		this.op = op;
+		this.term1 = term1;
+		this.term2 = term2;
+		this.op = op;
+		this.result = result;
+		this.missingTerm = missingTerm;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Override
+	public String toString() {
+		if(missingTerm == term.TERM_1) {
+		return "? " + op + " " + term2 + " = " + result;
+		}
+		else if (missingTerm.TERM_1 == term.TERM_2) {
+			return term2 + " " + op + " " + " ?" + " = " + result;
+		}
+		else {
+			return term1 + " " +term2 + " = " + result;
+		}
 	}
+	
+	
 
 }
---
+++ ConUHacks3/src/logic/term.java
@@ -0,0 +1,7 @@
+package logic;
+
+public enum term {
+		TERM_1,
+		TERM_2,
+		RESULT
+}
